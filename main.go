23f09e7a70c45de86ee044e80e690c18ee8fcf5e
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-09-10 00:33:35 -0500

    just made `main.go` a bland standin file

--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"fmt"
 	// "internal/http"
+	// "omglol/address"
+	// "omglol/profile"
 	// "omglol/cmd"
-	"omglol/profile"
 )
 
 func main() {
+	fmt.Println("cli.lol")
+
 	// http.Get("https://api.omg.lol/address/arkhamcookie/statuses/648d5e756cc10")
 	// cmd.Statuslog()
-	profile.Profile("email", "omg-lol@mail.arkhamcookie.com")
+	// profile.Profile("email", "omg-lol@mail.arkhamcookie.com")
 }
