bf99c8ff1eb8ca3019284e385fb0955ecb5d2dc9
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-09-18 20:32:52 -0500

    replaced old commented out usage internal/http
    I know I just need to create test files,
    but one thing at a time.

--- main.go
+++ main.go
@@ -2,16 +2,18 @@ package main
 
 import (
 	"fmt"
-	// "internal/http"
-	// "omglol/address"
-	// "omglol/profile"
-	// "omglol/cmd"
 )
 
 func main() {
 	fmt.Println("cli.lol")
 
-	// http.Get("https://api.omg.lol/address/arkhamcookie/statuses/648d5e756cc10")
+	/*
+	resp, err := http.Get("https://api.omg.lol/address/foobar/availability")
+	if err != nil {
+		log.Fatal(err)
+	}
+	println(resp)
+	*/
 
 	// address.Available("arkhamcookie")
 	// profile.Profile("email", "omg-lol@mail.arkhamcookie.com")
