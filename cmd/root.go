c941cdf08b27d4c3c7c470afd2763ea450840621
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-12-04 18:13:15 -0600

    Create root cmd

---
+++ cmd/root.go
@@ -0,0 +1,26 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "lol",
+	Short: "cli-lol is a commandline interface for omg.lol",
+	Long: `cli-lol is an open-source commandline interface for https://omg.lol built with Golang.
+		You can find the source code at https://git.theuntitledproject.dev/cli-lol,
+		and the documentation at https://docs.theuntitledproject.dev/cli-lol.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Do Stuff Here
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
