4ece8ad433de5d31662311c73283e1b634a81c1c
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-12-04 22:10:57 -0600

    Add verbose global flag

--- cmd/root.go
+++ cmd/root.go
@@ -17,6 +17,7 @@ var rootCmd = &cobra.Command{
 }
 
 func Execute() {
+	rootCmd.PersistentFlags().BoolVarP(&Verbose, "verbose", "v", false, "verbose output")
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)
