ab7ffdba13eeb0a6d20c3d31eddc244fccfb37fa
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-12-04 21:37:09 -0600

    Create available command using Cobra
    This is just a basic command that
    that will be under the address later.
    
    Creating it is more just to learn about the library,
    rather than the command itself.

---
+++ cmd/available.go
@@ -0,0 +1,27 @@
+package cmd
+
+import (
+	"fmt"
+	"omglol/address"
+
+	"github.com/spf13/cobra"
+)
+
+var availableCmd = &cobra.Command{
+	Use: "available <address>",
+	Short: "Get the availability of an address",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		response, err := address.Available(args[0])
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// TODO: Format the response before printing it
+		fmt.Println(response)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(availableCmd)
+}
