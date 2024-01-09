9f9a1817b23c32e8e8db87c5e48973ff0e33944c
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-01-09 09:16:39 -0600

    Add view command

--- cmd/statuslog.go
+++ cmd/statuslog.go
@@ -22,6 +22,28 @@ func listCmd(address string) {
 	println(result.Response.Statuses)
 }
 
+func viewCmd(address, status string) {
+	if address == "" {
+		println("Usage: lol status v[iew] <address> <status-id>")
+		return
+	}
+	if status == "" {
+		println("Usage: lol status v[iew] <address> <status-id>")
+		return
+	}
+
+	result, err := statuslog.Retrieve(address, status)
+	if err != nil {
+		println(err)
+		return
+	}
+
+	println(result.Response.Message, "\n")
+	println(result.Response.Status.Emoji, result.Response.Status.RelativeTime)
+	println(result.Response.Status.Content)
+	// println(result.Response.Status.ExternalURL) // Planned verbose response
+}
+
 var statuslogCmd = &cobra.Command{
 	Use:   "status <command> [flags]",
 	Short: "View or manage statuses",
@@ -37,11 +59,13 @@ var statuslogCmd = &cobra.Command{
 			} else {
 				listCmd("")
 			}
-		case "list":
-			if len(args) > 1 {
-				listCmd(args[1])
+		case "v":
+			fallthrough
+		case "view":
+			if len(args) >= 3 {
+				viewCmd(args[1], args[2])
 			} else {
-				listCmd("")
+				viewCmd("", "")
 			}
 		}
 	},
