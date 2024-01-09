fcee1083a760e3ae86c2aa850a923967fc3303cd
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-01-09 12:35:52 -0600

    add list type

--- lib/purl/purl.go
+++ lib/purl/purl.go
@@ -1,1 +1,16 @@
 package purl
+
+type purlListData struct {
+	Request struct {
+		StatusCode int  `json:"status_code"`
+		Success    bool `json:"success"`
+	} `json:"request"`
+	Response struct {
+		Message string `json:"message"`
+		Purls   []struct {
+			Name    string `json:"name"`
+			URL     string `json:"url"`
+			Counter int    `json:"counter"`
+		} `json:"purls"`
+	} `json:"response"`
+}
