738dab1e652647bd40210ef74dfc6cfa2bb42d57
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-01-09 13:07:45 -0600

    purl.list()

--- lib/purl/purl.go
+++ lib/purl/purl.go
@@ -1,5 +1,14 @@
 package purl
 
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
 type purlListData struct {
 	Request struct {
 		StatusCode int  `json:"status_code"`
@@ -13,4 +22,33 @@ type purlListData struct {
 			Counter int    `json:"counter"`
 		} `json:"purls"`
 	} `json:"response"`
+}
+
+func List(address string) (*purlListData, error) {
+	if address == "" {
+		return nil, errors.New("no address given")
+	}
+
+	target := fmt.Sprintf("https://api.omg.lol/address/%s/purls", address)
+	resp, err := http.Get(target)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var result purlListData
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, errors.New("could not unmarshal JSON")
+	}
+
+	if result.Request.StatusCode != 200 {
+		errorMsg := fmt.Sprintf("status code: %d", result.Request.StatusCode)
+		return nil, errors.New(errorMsg)
+	}
+
+	return &result, nil
 }
