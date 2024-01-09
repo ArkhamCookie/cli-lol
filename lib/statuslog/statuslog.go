7568a1fea3c3af71b2ac2b1b7c896374af195a71
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-01-09 09:39:24 -0600

    Add Latest func

--- lib/statuslog/statuslog.go
+++ lib/statuslog/statuslog.go
@@ -47,6 +47,25 @@ type StatuslogRetrieve struct {
 	} `json:"response"`
 }
 
+type StatuslogLatest struct {
+	Request struct {
+		StatusCode int  `json:"status_code"`
+		Success    bool `json:"success"`
+	} `json:"request"`
+	Response struct {
+		Message  string `json:"message"`
+		Statuses []struct {
+			ID           string `json:"id"`
+			Address      string `json:"address"`
+			Created      string `json:"created"`
+			RelativeTime string `json:"relative_time"`
+			Emoji        string `json:"emoji"`
+			Content      string `json:"content"`
+			ExternalURL  any    `json:"external_url"`
+		} `json:"statuses"`
+	} `json:"response"`
+}
+
 func Retrieve(address, status string) (*StatuslogRetrieve, error) {
 	if address == "" {
 		return nil, errors.New("no address given")
@@ -92,6 +111,27 @@ func List(address string) (*StatuslogList, error) {
 	body, _ := io.ReadAll(resp.Body)
 
 	var result StatuslogList
+	if err := json.Unmarshal(body, &result); err != nil {
+		return nil, errors.New("could not unmarshal JSON")
+	}
+
+	if result.Request.StatusCode != 200 {
+		errorMsg := fmt.Sprintf("status code: %d", result.Request.StatusCode)
+		return nil, errors.New(errorMsg)
+	}
+	return &result, nil
+}
+
+func Latest() (*StatuslogLatest, error){
+	resp, err := http.Get("https://api.omg.lol/statuslog/latest")
+	if err != nil {
+		errorMsg := fmt.Sprintf("could not create GET request: %s", err)
+		return nil, errors.New(errorMsg)
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+
+	var result StatuslogLatest
 	if err := json.Unmarshal(body, &result); err != nil {
 		return nil, errors.New("could not unmarshal JSON")
 	}
